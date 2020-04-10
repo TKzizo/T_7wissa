1245a2f82f63a6ffd6b273d8236814bfdfcb0cd5
Ammali Mouna <im_ammali@esi.dz>
2020-04-10 19:20:44 +0100

    Update messages
    +envoyer message(interface)
    +messages recues

--- lib/screens/home/map.dart
+++ lib/screens/home/map.dart
@@ -9,7 +9,8 @@ import 'package:myapp/services/database.dart';
 import 'package:provider/provider.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:myapp/services/auth.dart';
-
+import 'package:sliding_up_panel/sliding_up_panel.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 
 
 class MyHomePage extends StatefulWidget {
@@ -23,6 +24,7 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   final AuthService _auth = AuthService();
   final databaseReference = Firestore.instance;
+  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
 
   StreamSubscription _locationSubscription;
   Location _locationTracker = Location();
@@ -56,9 +58,9 @@ class _MyHomePageState extends State<MyHomePage> {
           circleId: CircleId("car"),
           radius: newLocalData.accuracy,
           zIndex: 1,
-          strokeColor: Colors.deepOrangeAccent,
+          strokeColor: Colors.deepOrange,
           center: latlng,
-          fillColor: Colors.deepOrangeAccent.withAlpha(70));
+          fillColor: Colors.deepOrange.withAlpha(70));
     });
   }
 
@@ -103,10 +105,39 @@ class _MyHomePageState extends State<MyHomePage> {
 
 
 
-
+/*COMPOSANTS*/ 
+Widget _mapWidget(){
+        return  Container(
+          child: Scaffold(
+           key: _scaffoldKey,
+            backgroundColor: Colors.white,           
+            body: GoogleMap(
+                initialCameraPosition: initialLocation,
+                markers: Set.of((marker != null) ? [marker] : []),
+                circles: Set.of((circle != null) ? [circle] : []),
+                onMapCreated: (GoogleMapController controller) {
+                  _controller = controller;
+                },
+              ),
+               floatingActionButton: FloatingActionButton(
+                child: Icon(Icons.location_searching,),
+                onPressed: () {
+                  getCurrentLocation();
+                },
+                backgroundColor: Colors.deepOrangeAccent,
+          ),
+       )               
+        );  
+    
+  }
 
   Widget build(BuildContext context) {
+    
     final user = Provider.of<User>(context);
+      BorderRadiusGeometry radius = BorderRadius.only(
+      topLeft: Radius.circular(30.0),
+      topRight: Radius.circular(30.0),
+    );
     return Scaffold(
 
       /*Bar*/
@@ -117,24 +148,26 @@ class _MyHomePageState extends State<MyHomePage> {
 
       )  ,
       /*MENU*/
-
-      body: GoogleMap(
-        mapType: MapType.hybrid,
-        initialCameraPosition: initialLocation,
-        markers: Set.of((marker != null) ? [marker] : []),
-        circles: Set.of((circle != null) ? [circle] : []),
-        onMapCreated: (GoogleMapController controller) {
-          _controller = controller;
-        },
-
+        bottomNavigationBar: BottomAppBar(
+      child: new Row(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          IconButton(icon: Icon(Icons.free_breakfast), onPressed: () {},),
+          IconButton(icon: Icon(Icons.message), onPressed: ()=> _onButtonPressed(),),
+          IconButton(icon: Icon(Icons.group ), onPressed: () {},),
+          IconButton(icon: Icon(Icons.place), onPressed: () {},),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.location_searching,),
-          onPressed: () {
-            getCurrentLocation();
-          },
-        backgroundColor: Colors.deepOrangeAccent,
-          ),
+    ),
+      body:   SlidingUpPanel(
+       backdropEnabled: true,
+      panelBuilder: (ScrollController sc) => _scrollingList(sc),
+     body: _mapWidget(),
+      
+       borderRadius: radius ,
+      ),
+     
 
       drawer: Drawer(
         child: Column(
@@ -210,8 +243,321 @@ class _MyHomePageState extends State<MyHomePage> {
 
 
   }
+_buildlistItem(BuildContext ctx,DocumentSnapshot document) {
+     return(ListTile(
+    title: Text(
+      document['sender']+":",
+      style: const TextStyle(
+          color:  const Color(0xff3d3d3d),
+          fontWeight: FontWeight.w400,
+          fontFamily: "Roboto",
+          fontStyle:  FontStyle.normal,
+          fontSize: 17.0
+      ),
+      textAlign: TextAlign.left                
+      ),
+     subtitle :   Text(
+                       document['message'],
+                      style: const TextStyle(
+                          color:  const Color(0xde3d3d3d),
+                          fontWeight: FontWeight.w400,
+                          fontFamily: "Roboto",
+                          fontStyle:  FontStyle.normal,
+                          fontSize: 14.0
+                      ),
+                      textAlign: TextAlign.left                
+                      ),
+         trailing:    Text(
+                       document['time'],
+                      style: const TextStyle(
+                          color:  const Color(0xde3d3d3d),
+                          fontWeight: FontWeight.w400,
+                          fontFamily: "Roboto",
+                          fontStyle:  FontStyle.normal,
+                          fontSize:12.0
+                      ),
+                      textAlign: TextAlign.left                
+                      ),
 
 
 
+
+                      )
+                  );
+  }
+  Widget _scrollingList(ScrollController sc){
+  return Container(
+  padding: EdgeInsets.symmetric(vertical:20.0,horizontal:40.0),
+  child:
+  ListView(
+    controller: sc,
+    children: <Widget>[
+   ListTile(
+   title:  Text(
+      'On a démarré !',
+      style: const TextStyle(
+          color:  const Color(0xff3d3d3d),
+          fontWeight: FontWeight.w400,
+          fontFamily: "Roboto",
+          fontStyle:  FontStyle.normal,
+          fontSize: 17.0
+      ),
+      textAlign: TextAlign.left                
+      ),
+   leading: Icon(Icons.departure_board),
+   trailing: Icon(Icons.send),
+
+   ),
+  ListTile(
+   title:  Text(
+      'Je suis en route !',
+      style: const TextStyle(
+          color:  const Color(0xff3d3d3d),
+          fontWeight: FontWeight.w400,
+          fontFamily: "Roboto",
+          fontStyle:  FontStyle.normal,
+          fontSize: 17.0
+      ),
+      textAlign: TextAlign.left                
+      ),
+   leading: Icon(Icons.directions_car),
+   trailing: Icon(Icons.send),
+
+   ),
+  ListTile(
+   title:  Text(
+      'Je suis arrivé !',
+      style: const TextStyle(
+          color:  const Color(0xff3d3d3d),
+          fontWeight: FontWeight.w400,
+          fontFamily: "Roboto",
+          fontStyle:  FontStyle.normal,
+          fontSize: 17.0
+      ),
+      textAlign: TextAlign.left                
+      ),
+   leading: Icon(Icons.arrow_drop_down_circle),
+   trailing: Icon(Icons.send),
+
+   ),
+  ListTile(
+   title:  Text(
+      'J ai besoin d aide',
+      style: const TextStyle(
+          color:  const Color(0xff3d3d3d),
+          fontWeight: FontWeight.w400,
+          fontFamily: "Roboto",
+          fontStyle:  FontStyle.normal,
+          fontSize: 17.0
+      ),
+      textAlign: TextAlign.left                
+      ),
+   leading: Icon(Icons.help),
+   trailing: Icon(Icons.send),
+
+   ),
+  ListTile(
+   title:  Text(
+      'Je suis en panne ! !',
+      style: const TextStyle(
+          color:  const Color(0xff3d3d3d),
+          fontWeight: FontWeight.w400,
+          fontFamily: "Roboto",
+          fontStyle:  FontStyle.normal,
+          fontSize: 17.0
+      ),
+      textAlign: TextAlign.left                
+      ),
+   leading: Icon(Icons.build),
+   trailing: Icon(Icons.send),
+
+   ),
+   ListTile(
+   title:  Text(
+      'un accident !',
+      style: const TextStyle(
+          color:  const Color(0xff3d3d3d),
+          fontWeight: FontWeight.w400,
+          fontFamily: "Roboto",
+          fontStyle:  FontStyle.normal,
+          fontSize: 17.0
+      ),
+      textAlign: TextAlign.left                
+      ),
+   
+   trailing: Icon(Icons.send),
+
+   ),
+   ListTile(
+   title:  Text(
+      'Route endomagée !',
+      style: const TextStyle(
+          color:  const Color(0xff3d3d3d),
+          fontWeight: FontWeight.w400,
+          fontFamily: "Roboto",
+          fontStyle:  FontStyle.normal,
+          fontSize: 17.0
+      ),
+      textAlign: TextAlign.left                
+      ),
+   
+   trailing: Icon(Icons.send),
+
+   ),
+   ListTile(
+   title:  Text(
+      'Alerte barage !',
+      style: const TextStyle(
+          color:  const Color(0xff3d3d3d),
+          fontWeight: FontWeight.w400,
+          fontFamily: "Roboto",
+          fontStyle:  FontStyle.normal,
+          fontSize: 17.0
+      ),
+      textAlign: TextAlign.left                
+      ),
+   
+   trailing: Icon(Icons.send),
+
+   ),
+   ListTile(
+   title:  Text(
+      'Alerte radar !',
+      style: const TextStyle(
+          color:  const Color(0xff3d3d3d),
+          fontWeight: FontWeight.w400,
+          fontFamily: "Roboto",
+          fontStyle:  FontStyle.normal,
+          fontSize: 17.0
+      ),
+      textAlign: TextAlign.left                
+      ),
+   leading: Icon(Icons.flag),
+   trailing: Icon(Icons.send),
+
+   ),
+   ListTile(
+   title:  Text(
+      'Appelez moi !',
+      style: const TextStyle(
+          color:  const Color(0xff3d3d3d),
+          fontWeight: FontWeight.w400,
+          fontFamily: "Roboto",
+          fontStyle:  FontStyle.normal,
+          fontSize: 17.0
+      ),
+      textAlign: TextAlign.left                
+      ),
+   leading: Icon(Icons.call),
+   trailing: Icon(Icons.send),
+
+   ),
+  ListTile(
+   title:  Text(
+      'OK!',
+      style: const TextStyle(
+          color:  const Color(0xff3d3d3d),
+          fontWeight: FontWeight.w400,
+          fontFamily: "Roboto",
+          fontStyle:  FontStyle.normal,
+          fontSize: 17.0
+      ),
+      textAlign: TextAlign.left                
+      ),
+   leading: Icon(Icons.check),
+   trailing: FlatButton(
+     child: Icon(
+       Icons.send, 
+     ),
+     onPressed: (){}, ),
+
+   ),
+  
+    ],
+    
+  ));
+ 
+}
+void _onButtonPressed(){
+   
+    showModalBottomSheet(context: context, builder:(context){
+     return Container(
+        color: const Color(0xff737373),
+       width: 360,
+      height: 535,
+      child:Container(
+      decoration: BoxDecoration(
+       color: const Color(0xffffffff),
+      borderRadius:  BorderRadius.only(
+          topLeft:  const Radius.circular(30) ,
+          topRight:  const Radius.circular(30) ,
+        ),
+      ),
+      
+       child: Stack(children: [
+  // ✏️ Headline 6 
+  PositionedDirectional(
+    top: 35,
+    start: 38,
+    child: 
+        SizedBox(
+      width: 77,
+      height: 26,
+      child: Text(
+      "Messages ",
+      style: const TextStyle(
+          color:  const Color(0xde204f6f),
+          fontWeight: FontWeight.w500,
+          fontFamily: "Roboto",
+          fontStyle:  FontStyle.normal,
+          fontSize: 19.0
+      ),
+      textAlign: TextAlign.left                
+      )),
+  ),
+     
+     Container( 
+     padding: EdgeInsets.symmetric(vertical:65.0,horizontal :20.0),
+     child: StreamBuilder(
+     stream: Firestore.instance.collection('messagerie').snapshots(),
+     builder: (context,snapshot){
+     if (!snapshot.hasData) return const Text("aucun message",
+      style: const TextStyle(
+      color:  const Color(0xff3d3d3d),
+      fontWeight: FontWeight.w400,
+      fontFamily: "Roboto",
+      fontStyle:  FontStyle.normal,
+      fontSize: 17.0
+  ),
+  textAlign: TextAlign.left 
+     
+     
+     );
+   return  ListView.builder(
+     itemExtent: 80.0,
+     itemCount:snapshot.data.documents.length,
+    itemBuilder: (ctx,index )=> (
+    _buildlistItem(ctx,snapshot.data.documents[index])),
+      );
+    
+     }
+         
+      )
+    
+       
+      ) , 
+      
+      ]
+      )
+         
+          ),
+        
+          );
+          
+    
+        }
+        );
+         
+      }
 
 }
